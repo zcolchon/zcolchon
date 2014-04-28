35f52355339cef917ce9d49dd91449a36509e7aa
zcolchon <zcolchon@gmail.com>
2014-04-27 23:13:31 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>zcolchon</title>
+		<meta description="zcolchon - Zacarías Colchón Córdova, me apasiona las tecnologias, realizo desarrollo web, y más... ">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<div>
+			<p>
+				<span class="tit">&#60;zcolchon&#62;</span>
+				<span class="descr">Web Developer</span>
+				<span class="otro">"sitio en construcción"</span>
+				<span class="tit">&#60;/zcolchon&#62;</span>
+			</p>
+		</div>		
+	</body>
+</html>
